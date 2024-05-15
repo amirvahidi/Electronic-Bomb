90e4171f852edab1ebf79c9c420b34cb87400107
amirvahidi <amirvahidi83@gmail.com>
2024-05-15 13:10:50 +0330

    fix tank animation

--- src/main/java/model/Tank.java
+++ src/main/java/model/Tank.java
@@ -6,5 +6,8 @@ public class Tank extends DynamicTarget {
 
 	public Tank(double x, double y, boolean goingRight){
 		super(x, y, Constant.TANK_WIDTH.getValue(), Constant.TANK_HEIGHT.getValue(),  goingRight, Constant.TANK_SPEED.getValue());
+		if (!goingRight){
+			this.setScaleX(-1);
+		}
 	}
 }
